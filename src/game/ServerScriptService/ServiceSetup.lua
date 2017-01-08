397b6f607da4fb59874751c0e13621a9e91d4733
David Minnerly <dtreeLive@hotmail.com>
2017-01-08 09:54:14 -0500

    Merge SetupRemoteAccess into ServiceSetup
    These two are small enough that combining them is beneficial. We only need to setup remote access from the server, and we never use this functionality anywhere else.

--- src/game/ServerScriptService/ServiceSetup.lua
+++ src/game/ServerScriptService/ServiceSetup.lua
@@ -13,12 +13,54 @@
 --]]
 
 local replicatedStorage = game:GetService("ReplicatedStorage")
-
 local services = replicatedStorage.Services
-local setupRemoteAccess = require(services.Modules.SetupRemoteAccess)
 
 local function isAService(obj)
   return obj:IsA("ModuleScript") and string.match(obj.Name:lower(), "service$")
+end
+
+-- Gets all the methods from a table.
+local function getMethods(t)
+  local methods = {}
+  for key, value in pairs(t) do
+    if type(value) == "function" then
+      methods[key] = value
+    end
+  end
+  return methods
+end
+
+local function newRemoteFunction(name, parent, serviceTable, callback)
+  --[[ The first argument is the player, which we purposefully ignore.
+
+    Our services work like ROBLOX's built in ones, which never have the player
+    automatically passed to them.  If a service needs access to a player, it
+    can be passed in manually. ]]
+  local function invoke(_, ...)
+    -- We pass in serviceTable to propagate the Service's `self`,
+    return callback(serviceTable, ...)
+  end
+
+  local remote = Instance.new("RemoteFunction")
+  remote.Name = name
+  remote.OnServerInvoke = invoke
+  remote.Parent = parent
+
+  return remote
+end
+
+local function replicateMethods(methods, remoteStorage, serviceTable)
+  for name, callback in pairs(methods) do
+    newRemoteFunction(name, remoteStorage, serviceTable, callback)
+  end
+end
+
+local function setupRemoteAccess(serviceModule)
+  local serviceTable = require(serviceModule)
+  local methods = getMethods(serviceTable)
+  local remoteStorage = storage.getMethods(serviceModule)
+
+  replicateMethods(methods, remoteStorage, serviceTable)
 end
 
 local function init()
